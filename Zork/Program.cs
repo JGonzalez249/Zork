d73d57625ba0423bb545f832b3c724f6b1833053
Jonathan Gonzalez <jgonzalez249@gmail.com>
2020-09-15 22:36:33 -0400

    Zork 1.3
    Closest I could get to the assignment output without errors or warnings

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,19 +1,55 @@
 ﻿using System;
-using System.Linq.Expressions;
 
 namespace Zork
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString = "";
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        Console.WriteLine($"A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        break;
+
+                    case Commands.EAST:
+                        Console.WriteLine($"You moved {command}");
+                        break;
+
+                    case Commands.NORTH:
+                        Console.WriteLine($"You moved {command}");
+                        break;
+
+                    case Commands.SOUTH:
+                        Console.WriteLine($"You moved {command}");
+                        break;
+
+                    case Commands.WEST:
+                        Console.WriteLine($"You moved {command}");
+                        break;
+
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown command.");
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;  
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
