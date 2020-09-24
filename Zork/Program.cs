bc792820dbbd4f71cb6e90944a59434381ac38bb
Jonathan Gonzalez <jgonzalez249@gmail.com>
2020-09-24 18:55:08 -0400

    Zork 2.1
    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -10,7 +10,7 @@ namespace Zork
 
             get
             {
-                return Rooms[LocationColumn]; //Gives current location of the player
+                return Rooms[Location.Row, Location.Column]; //Gives current location of the player
             }
         }
 
@@ -25,18 +25,17 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                       Console.WriteLine( "This is an open field west of a white house, with a boarded front door.");
                         break;
 
                     case Commands.EAST:
                     case Commands.WEST:
                     case Commands.NORTH:
                     case Commands.SOUTH:
-                        outputString = $"You moved {command}";
+
                         if(Move(command) == false)
                         {
                             
@@ -45,15 +44,14 @@ namespace Zork
                         break;
 
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
 
-                Console.WriteLine(outputString);
             }
         }
 
@@ -63,14 +61,21 @@ namespace Zork
             bool isValidMove = true;
             switch (command)
             {
-                case Commands.EAST when LocationColumn < Rooms.GetLength(0) - 1:
-                    LocationColumn++;
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
                     break;
 
-                case Commands.WEST when LocationColumn > 0:
-                    LocationColumn--;
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
                     break;
 
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
+                    break;
+
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
 
                 default:
                     isValidMove = false;
@@ -81,9 +86,14 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" }; //Zork Map
+        private static readonly string[,] Rooms = {     //Zork Map
+            { "Rocky Trail", "South of House", "Canyon View"},
+            { "Forest", "West of House", "Behind House"},
+            { "Dense Woods", "North of House","Clearing"},
+        
+        };
 
-        private static int LocationColumn = 1;
+        private static (int Row, int Column) Location = (1, 1); //Player Spawns West of House
 
     }
 
