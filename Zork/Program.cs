ca987bfaa2aae847b41177121551194a322c7413
Jonathan Gonzalez <jgonzalez249@gmail.com>
2020-10-01 17:25:33 -0400

    Zork 3.1
    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,7 +5,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom 
+        public static Room CurrentRoom 
         {
 
             get
@@ -14,9 +14,12 @@ namespace Zork
             }
         }
 
+       
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)  //Game Loop
@@ -28,7 +31,7 @@ namespace Zork
                 switch (command)
                 {
                     case Commands.LOOK:
-                       Console.WriteLine( "This is an open field west of a white house, with a boarded front door.");
+                       Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.EAST:
@@ -54,6 +57,7 @@ namespace Zork
 
             }
         }
+
 
         private static bool Move(Commands command)
         {
@@ -89,19 +93,37 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly string[,] Rooms = {     //Zork Map
-            { "Rocky Trail", "South of House", "Canyon View"},
-            { "Forest", "West of House", "Behind House"},
-            { "Dense Woods", "North of House","Clearing"},
+        //Zork Map
+
+        private static readonly Room[,] Rooms = {    
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House")},
+            { new Room("Dense Woods"), new Room("North of House"),new Room("Clearing")},
         
         };
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";                                                                                // Rocky Trial
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all windows are barred.";             // South of House
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                      // Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                     // Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                        // West of House
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";     // Behind House
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";            // Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all windows are barred.";             // North of House
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                    // Clearing
+        }
 
         private static readonly List<Commands> Directions = new List<Commands>
         {
             Commands.NORTH,
             Commands.SOUTH,
             Commands.EAST,
-            Commands.WEST
+            Commands.WEST,
+            Commands.LOOK
         };
 
         private static (int Row, int Column) Location = (1, 1); //Player Spawns West of House on array
